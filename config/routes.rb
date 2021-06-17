ebb0449440a2553231b5d5628a372d243ee2be59
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-06-16 17:22:36 -0700

    attempt to resolve webpack issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'sessions#welcome'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   resources :pages
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
